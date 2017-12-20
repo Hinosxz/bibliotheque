234b34b6cf8a2e057b51b966542fcc6b6a6506e6
Gauthier <Hinosxz@MacBook-Pro-de-Gauthier.local>
2017-12-20 11:07:34 +0100

    Adherent est completé, reste à implémenter Bibliothèque

---
+++ src/Adherent.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "Adherent.h"
+#include "Bibliotheque.h"
+#include <string>
+using namespace std;
+
+Adherent::Adherent(int id_adherent, string nom, string prenom, string adresse, Bibliotheque biblio_affiliee, int nb_livres_max){
+    this->id_adherent = id_adherent;
+    this->nom = nom;
+    this->prenom = prenom;
+    this->adresse = adresse;
+    this->biblio_affiliee = biblio_affiliee;
+    this->nb_livres_max = nb_livres_max;
+    this->liste_livres_empruntes = new int[nb_livres_max];
+    this->curseur = 0;
+}
+
+void Adherent::emprunte(id_livre){
+    if (curseur < nb_livres_max){
+        liste_livres_empruntes[curseur] = id_livre; /* Ajoute l'id du livre emprunté à la liste à la position curseur */
+        curseur++; /* Incrémente le curseur */
+        biblio_affiliee.prete(id_livre);
+    }
+    else{
+        cout << "Désolé, vous avez atteint le nombre maximum de livres empruntés.";
+    }
+}
+
+void Adherent::rend(id_livre){
+    /* Recherche le livre dans la liste */
+    int indice;
+    for (int i = 0; i < nb_livres_max; i++){
+        if (liste_livres_empruntes[i] == id_livre){
+            indice = i;
+        }
+    }
+    /* Recopie l'ancienne liste sans le livre en question dans une nouvelle */
+    int *nouvelle_liste = new int[nb_livres_max];
+    /* On recopie la première partie de la liste jusqu'à l'indice du livre à supprimer */
+    for (int i = 0; i < indice; i++){
+        nouvelle_liste[i] = liste_livres_empruntes[i];
+    }
+    /* On recopie la seconde partie de la liste jusqu'à la fin en sautant le livre liste_livres_empruntes[indice] */
+    for (int i = indice; i < curseur - 1; i++){
+        nouvelle_liste[i] = liste_livres_empruntes[i+1];
+    }
+    /* On change la liste de livres empruntés par la nouvelle liste */
+    liste_livres_empruntes = nouvelle_liste;
+    /* On décrémente le curseur */
+    curseur--;
+    /* La bibliothèque reprend le livre */
+    biblio_affiliee.reprend(id_livre);
+}
---
+++ src/Adherent.h
@@ -0,0 +1,25 @@
+#include <string>
+#include "Livre.h"
+#include "Bibliotheque.h"
+using namespace std;
+
+#ifndef ADHERENT_H_
+#define ADHERENT_H_
+
+class Adherent{
+private:
+    int id_adherent;
+    string nom;
+    string prenom;
+    string adresse;
+    Bibliotheque &biblio_affiliee; /* référence vers la bibliothèque affiliée */
+    Livre *liste_livres_empruntes; /* liste contient les livres empruntes*/
+    int nb_livres_max;
+    int curseur;
+public:
+    Adherent(int id_adherent, string nom, string prenom, string adresse, Bibliotheque biblio_affiliee, int nb_livres_max);
+    void emprunte(id_livre);
+    void rend(id_livre);
+};
+
+#endif /* ADHERENT_H_ */
